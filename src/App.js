01d1a5657498164dc23b2aabb188256802a6d7b2
diloloan72 <diloloan72@gmail.com>
2022-02-20 23:33:21 -0800

    Connect app to server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React, {useState, useCallback} from 'react';
 import "./App.css";
 import ImageUploader from "./ImageUploader.js"
 
 function App() {
+  const [logos, setLogos] = useState('Detecting');
+
+  const detectLogos = useCallback(async () => {
+    await fetch('/detect_logos').then(res => res.json()).then(data => {
+      setLogos(data.logos);
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>Loan yeu Derrick</h1>
+      <h2>{logos}</h2>
       <ImageUploader />
-      <button type="button">Detect logo</button>
+      <button type="button" onClick={detectLogos}>Detect logo</button>
     </div>
   );
 }
